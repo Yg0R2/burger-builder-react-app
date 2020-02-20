b95a0a29ac805dbc3cd87cc5a30f5b0a63cf9682
Yg0R2 <yg0r2@yahoo.com>
2020-02-20 15:23:18 +0100

    Create custom HTML element with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Person from './Person/Person'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi there.</h1>
+      <Person name="Szilvia" age="33" />
+      <Person name="Tibor" age="35" />
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi there.'));
