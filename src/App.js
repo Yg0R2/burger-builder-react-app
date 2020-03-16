265743d49fba14667dd3c6f4a034f2bcdca3bc3e
Yg0R2 <yg0r2@yahoo.com>
2020-03-16 18:19:13 +0100

    Implement Burger skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Layout from './components/Layout/Layout';
+
 class App extends React.Component {
 
   render() {
     return (
       <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,56 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+import styles from './BurgerIngredient.module.css';
+
+class BurgerIngredient extends React.Component {
+
+  render() {
+    let ingredient = null;
+
+    switch (this.props.type) {
+      case ('bacon'): {
+        ingredient = <div className={styles.bacon}></div>;
+        break;
+      }
+      case ('bread-bottom'): {
+        ingredient = <div className={styles.breadBottom}></div>;
+        break;
+      }
+      case ('bread-top'): {
+        ingredient = (
+          <div className={styles.breadTop}>
+            <div className={styles.seeds1}></div>
+            <div className={styles.seeds2}></div>
+          </div>
+        );
+        break;
+      }
+      case ('cheese'): {
+        ingredient = <div className={styles.cheese}></div>;
+        break;
+      }
+      case ('meat'): {
+        ingredient = <div className={styles.meat}></div>;
+        break;
+      }
+      case ('salad'): {
+        ingredient = <div className={styles.salad}></div>;
+        break;
+      }
+      default: {
+        ingredient = null;
+        break;
+      }
+    }
+
+    return ingredient;
+  }
+
+}
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+};
+
+export default BurgerIngredient;
