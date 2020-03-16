1faf94b28121e6094a1634f8dd0cd38327dad1d5
Yg0R2 <yg0r2@yahoo.com>
2020-03-16 18:40:42 +0100

    Transform object to array or "error" message

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,13 +6,20 @@ import styles from './Burger.module.css'
 
 const burger = (props) => {
 
-  const ingredientsArray = Object.keys(props.ingredients)
+  let ingredientsArray = Object.keys(props.ingredients)
     .map(type => {
       return [...Array(props.ingredients[type])]
         .map((_, index) => {
           return <BurgerIngredient key={type + index} type={type} />;
         })
-    });
+    })
+    .reduce((previous, current) => {
+      return previous.concat(current);
+    }, []);
+
+  if (ingredientsArray.length === 0) {
+    ingredientsArray = <p>Please start add ingredients!</p>;
+  }
 
   return (
     <div className={styles.burger}>
