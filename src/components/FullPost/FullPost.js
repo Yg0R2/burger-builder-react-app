28736cf3257a2a7d68a58402bb19ac0fa19b095c
Yg0R2 <yg0r2@yahoo.com>
2020-03-17 17:39:03 +0100

    Use Axios global configuration

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -14,7 +14,7 @@ class FullPost extends Component {
   // Do not fetch data from server if the required data is already there.
   componentDidUpdate(prevProps, prevState, snapshot) {
     if (this.props.id && (!this.state.loadedPost || (this.state.loadedPost && (this.state.loadedPost.id !== this.props.id)))) {
-      axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+      axios.get('/posts/' + this.props.id)
         .then(response => {
           this.setState({loadedPost: response.data})
         });
@@ -22,7 +22,7 @@ class FullPost extends Component {
   }
 
   deletePostHandler = () => {
-    axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+    axios.delete('/posts/' + this.props.id)
       .then(response => {
         console.log(response);
       });
