de6b2a7ed853b78ca5bce59411567a795ba2c0ee
Yg0R2 <yg0r2@yahoo.com>
2020-03-17 17:27:29 +0100

    Error handling globally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -17,7 +17,7 @@ class Blog extends Component {
   };
 
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/postssss')
+    axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
         const posts = response.data.slice(0, 4)
           .map(post => {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import axios from 'axios';
+
 import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
+
+axios.interceptors.request.use(request => {
+  console.log(request);
+
+  return request;
+}, error => {
+  // Handle errors when the request cannot be sent
+  console.log(error);
+
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  console.log(response);
+
+  return response;
+}, error => {
+  console.log(error);
+
+  return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
