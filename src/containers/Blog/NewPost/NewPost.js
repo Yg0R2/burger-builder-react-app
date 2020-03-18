b1ee7758064a5831ba090a25533988c57e99516b
Yg0R2 <yg0r2@yahoo.com>
2020-03-18 16:37:05 +0100

    Alternative "redirect"

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Redirect} from 'react-router-dom';
 
 import axios from 'axios';
 
@@ -10,8 +9,7 @@ class NewPost extends Component {
   state = {
     title: '',
     content: '',
-    author: 'Max',
-    submitted: false
+    author: 'Max'
   };
 
   componentDidMount() {
@@ -28,14 +26,15 @@ class NewPost extends Component {
     axios.post('/posts', post)
       .then(response => {
         console.log(response);
-        this.setState({submitted: true});
+
+        this.props.history.replace('/posts');
+        //this.setState({submitted: true});
       });
   };
 
   render() {
     return (
       <div className="NewPost">
-        {this.state.submitted ? <Redirect to="/posts" /> : null}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
