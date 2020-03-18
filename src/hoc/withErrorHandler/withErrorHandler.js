65e319ddde014dfd78307ee81437b2799a8b8bd8
Yg0R2 <yg0r2@yahoo.com>
2020-03-18 11:51:08 +0100

    Preventing memory leak with remove interceptors during component unmount

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -12,16 +12,22 @@ const withErrorHandler = (WrappedComponent, axios) => {
     constructor(props) {
       super(props);
 
-      axios.interceptors.request.use(request => {
+      this.requestInterceptor = axios.interceptors.request.use(request => {
         this.setState({error: null});
         return request;
       });
 
-      axios.interceptors.response.use(
+      this.responseInterceptor = axios.interceptors.response.use(
         response => response,
         error => {
           this.setState({error: error});
         });
+    }
+
+    // Do not forget to remove interceptors!
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.requestInterceptor);
+      axios.interceptors.response.eject(this.responseInterceptor);
     }
 
     errorConfirmedHandler = () => {
