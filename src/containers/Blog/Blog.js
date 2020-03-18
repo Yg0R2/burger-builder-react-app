ba70c6acde36f8e97cc1bce08c9e6dc38fc416ef
Yg0R2 <yg0r2@yahoo.com>
2020-03-18 13:22:02 +0100

    Prevent pages to reload

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 
 import Posts from '../../containers/Blog/Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -14,8 +14,15 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to={{
+                pathname: 'new-post',
+                hash: '#submit',
+                search: '?param1=value1'
+              }}
+              >
+                New Post
+              </Link></li>
             </ul>
           </nav>
         </header>
