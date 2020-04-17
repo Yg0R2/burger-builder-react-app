90e23c63199fb5a32d133d274b0d2590b251611c
Yg0R2 <yg0r2@yahoo.com>
2020-04-17 20:52:33 +0200

    Fetching orders from backend

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 import Order from '../../components/Order/Order';
 
+import axiosOrders from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
 class Orders extends React.Component {
+
+  state = {
+    orders: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    axiosOrders.get('/orders.json')
+      .then(result => {
+        const fetchedOrder = [];
+        for (let key in result.data) {
+          fetchedOrder.push({
+            ...result.data[key],
+            id: key
+          });
+        }
+
+        this.setState({loading: false, orders: fetchedOrder});
+      })
+      .catch(error => {
+        this.setState({loading: false});
+      })
+  }
 
   render() {
     return (
@@ -15,4 +41,4 @@ class Orders extends React.Component {
 
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axiosOrders);
