33df1a23563634b197bb47fc4349a1aeba31309b
Yg0R2 <yg0r2@yahoo.com>
2020-05-27 16:19:20 +0200

    Initialize store/reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore} from 'redux';
+
 import App from './App';
+
+import reducer from './store/reducer';
 import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
