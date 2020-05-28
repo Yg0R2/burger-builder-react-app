d2434d05cc1ce2035878d85a9389b4fe8bd21f88
Yg0R2 <yg0r2@yahoo.com>
2020-05-28 18:03:36 +0200

    Connect to store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 
 import App from './App';
@@ -9,7 +10,11 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
-const store = createStore(reducer);
+const app = (
+  <Provider store={createStore(reducer)}>
+    <App />
+  </Provider>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
