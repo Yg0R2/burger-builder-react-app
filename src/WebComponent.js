70ff078b83c72f7abe1161f7bf9c729bff75fea4
Yg0R2 <yg0r2@yahoo.com>
2020-06-16 16:45:32 +0200

    add css

--- src/WebComponent.js
+++ src/WebComponent.js
@@ -6,9 +6,24 @@ import styles from './WebContent.css';
 class WebComponent extends HTMLElement {
   connectedCallback() {
     const mountPoint = document.createElement('div');
+    mountPoint.setAttribute('class', 'webContent')
 
     const shadow = this.attachShadow({mode: 'open'});
     shadow.appendChild(mountPoint);
+
+    // Create some CSS to apply to the shadow dom
+    let style = document.createElement('style');
+    style.textContent =
+      `.webContent {
+        color: greenyellow;
+        background-color: green;
+      }
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+      `;
+    shadow.appendChild(style);
 
     const name = this.getAttribute('name');
     const url = 'https://www.google.com/search?q=' + encodeURIComponent(name);
