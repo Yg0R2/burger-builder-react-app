1e6fe8b927f89478d550278d05bf5bddcd11cbfa
Yg0R2 <yg0r2@yahoo.com>
2020-06-17 16:40:02 +0200

    my-loader not working, it seems this is the only loader which is running

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,31 @@
+const path = require('path');
+
 const logWebpackConfigPlugin = require("./craco-plugin-log-webpack-config");
 
 module.exports = {
   webpack: {
     configure: {
+      //Doesnt load other loaders because of this
+      resolveLoader: {
+        modules: [
+          path.resolve(__dirname, './src/loaders')
+        ]
+      },
       module: {
         rules: [
-          /*{
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: './src/loaders/my-loader'
+              }
+            ]
+          }
+        ]
+      }
+      /*module: {
+        rules: [
+          {
             test: /\.css$/,
             use: [
               {
@@ -15,9 +35,9 @@ module.exports = {
                 }
               }
             ]
-          }*/
+          }
         ]
-      }
+      }*/
     }
   },
   plugins: [
---
+++ src/loaders/my-loader.js
@@ -0,0 +1,37 @@
+const loaderUtils = require('loader-utils');
+const validateOptions = require('schema-utils');
+const styleLoader = require("style-loader");
+
+module.exports.pitch = function (request) {
+  return request;
+
+  /*let content = JSON.stringify(require(request));
+
+  if(typeof content === 'string') {
+    content = [[module.id, content, '']];
+  }
+
+  /*const options = {};
+  let update = require("./addStyles.js")(content, options);
+
+  if(content.locals) {
+    module.exports = content.locals;
+  }
+
+  if(module.hot) {
+    if(!content.locals) {
+      module.hot.accept(loaderUtils.stringifyRequest(this, "!!" + request), function() {
+        let newContent = require(loaderUtils.stringifyRequest(this, "!!" + request));
+          if(typeof newContent === 'string') {
+            newContent = [[module.id, newContent, '']];
+          }
+
+          update(newContent);
+      });
+    }
+
+    module.hot.dispose(function() {
+      update();
+    });
+  }*/
+}
